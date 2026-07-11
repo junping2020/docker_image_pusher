e1f6867c91ca46575a03e37bc9ac481817db738c
junping2020 <54197928+junping2020@users.noreply.github.com>
2026-07-11 15:08:12 +0800

    Add files via upload

---
+++ .github/workflows/docker-sync.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+
+echo "=============================================================================="
+echo "Before freeing up disk space"
+df -hT
+echo "=============================================================================="
+
+docker login -u $ALIYUN_REGISTRY_USER -p $ALIYUN_REGISTRY_PASSWORD $ALIYUN_REGISTRY
+
+declare -A duplicate_images
+declare -A temp_map
+while IFS= read -r line || [ -n "$line" ]; do
+  [[ -z "$line" ]] && continue
+  if echo "$line" | grep -q '^\s*#'; then continue; fi
+  image=$(echo "$line" | awk '{print $NF}')
+  image="${image%%@*}"
+  echo "image $image"
+  image_name_tag=$(echo "$image" | awk -F'/' '{print $NF}')
+  echo "image_name_tag $image_name_tag"
+  name_space=$(echo "$image" | awk -F'/' '{if (NF==3) print $2; else if (NF==2) print $1; else print ""}')
+  echo "name_space: $name_space"
+  name_space="${name_space}_"
+  image_name=$(echo "$image_name_tag" | awk -F':' '{print $1}')
+  echo "image_name: $image_name"
+  if [[ -n "${temp_map[$image_name]}" ]]; then
+    if [[ "${temp_map[$image_name]}" != $name_space ]]; then
+      echo "duplicate image name: $image_name"
+      duplicate_images[$image_name]="true"
+    fi
+  else
+    temp_map[$image_name]=$name_space
+  fi
+done < images.txt
+
+while IFS= read -r line || [ -n "$line" ]; do
+  [[ -z "$line" ]] && continue
+  if echo "$line" | grep -q '^\s*#'; then continue; fi
+  echo "docker pull $line"
+  docker pull $line
+  platform=$(echo "$line" | awk -F'--platform[ =]' '{if (NF>1) print $2}' | awk '{print $1}')
+  echo "platform is $platform"
+  if [ -z "$platform" ]; then
+    platform_prefix=""
+  else
+    platform_prefix="${platform//\//_}_"
+  fi
+  echo "platform_prefix is $platform_prefix"
+  image=$(echo "$line" | awk '{print $NF}')
+  image_name_tag=$(echo "$image" | awk -F'/' '{print $NF}')
+  name_space=$(echo "$image" | awk -F'/' '{if (NF==3) print $2; else if (NF==2) print $1; else print ""}')
+  image_name=$(echo "$image_name_tag" | awk -F':' '{print $1}')
+  name_space_prefix=""
+  if [[ -n "${duplicate_images[$image_name]}" ]]; then
+    if [[ -n "${name_space}" ]]; then
+      name_space_prefix="${name_space}_"
+    fi
+  fi
+  image_name_tag="${image_name_tag%%@*}"
+  new_image="$ALIYUN_REGISTRY/$ALIYUN_NAME_SPACE/$platform_prefix$name_space_prefix$image_name_tag"
+  echo "docker tag $image $new_image"
+  docker tag $image $new_image
+  echo "docker push $new_image"
+  docker push $new_image
+  echo "=============================================================================="
+  df -hT
+  echo "=============================================================================="
+  docker rmi $image
+  docker rmi $new_image
+done < images.txt
+
+echo "Done"
+df -hT
